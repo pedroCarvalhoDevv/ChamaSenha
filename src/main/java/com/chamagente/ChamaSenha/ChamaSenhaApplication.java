b8eb838b61653dd9f5f3c171e0743e7ff7373d09
Pedro Carvalho <pedrozaugustoz@gmail.com>
2025-07-11 12:27:42 -0300

    Primeiro Commit

---
+++ src/main/java/com/chamagente/ChamaSenha/ChamaSenhaApplication.java
@@ -0,0 +1,13 @@
+package com.chamagente.ChamaSenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamaSenhaApplication.class, args);
+	}
+
+}
